b5e653021f800bb8fb39525c099595458b332327
DarshanVarpe <darshanvarpe@gmail.com>
2026-03-23 22:12:22 +0530

    fix(api): import revalidatePath cache library

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -1,5 +1,6 @@
 import { generateText } from "ai";
 import { google } from "@ai-sdk/google";
+import { revalidatePath } from "next/cache";
 
 import { db } from "@/firebase/admin";
 import { getRandomInterviewCover } from "@/lib/utils";
